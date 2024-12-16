8f52fb41f3de4af80c1e41d04c01745a469e89df
Brian Misachi <misachibrian@gmail.com>
2024-12-16 13:07:10 +0300

    Generic implementation for `fallocate`: For platforms where it is not supported

---
+++ fallocate.go
@@ -0,0 +1,37 @@
+//go:build !linux
+
+package main
+
+import (
+	"log"
+	"os"
+)
+
+const PAGE_SIZE int = 500
+
+func fallocate(fd int, offset, size int64) {
+	buf := [PAGE_SIZE]byte{}
+	file := os.NewFile(uintptr(fd), "falloc")
+
+	if file == nil {
+		log.Printf("Invalid file descriptor: %d\n", fd)
+		return
+	}
+
+	if (size - offset) < int64(PAGE_SIZE) {
+		log.Printf("Allocation region should be at least %d bytes\n", PAGE_SIZE)
+		goto end
+	}
+
+	if ((size - offset) % int64(PAGE_SIZE)) != 0 {
+		log.Printf("Allocation region must be PAGE_SIZE=%d aligned\n", PAGE_SIZE)
+		goto end
+	}
+
+	for i := offset; i < size; i += int64(PAGE_SIZE) {
+		file.Write(buf[:])
+	}
+
+end:
+	// file.Close()
+}
